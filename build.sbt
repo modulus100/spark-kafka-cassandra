0864a2d7a852bc9c121eda412f6dcbacee4c6eb6
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2022-04-23 16:01:52 +0300

    examples with kafka, cassandra and terminal

--- build.sbt
+++ build.sbt
@@ -1,14 +1,21 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.8"
+ThisBuild / scalaVersion := "2.12.15"
 
 lazy val root = (project in file("."))
   .settings(
     name := "spark-kafka-cassandra"
   )
 
+// spark
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "3.2.1",
   "org.apache.spark" %% "spark-sql" % "3.2.1",
-  "org.apache.spark" %% "spark-streaming" % "3.2.1"
+  "org.apache.spark" %% "spark-streaming" % "3.2.1",
+  "org.apache.spark" %% "spark-sql-kafka-0-10" % "3.2.1"
+)
+
+libraryDependencies ++= Seq(
+  "com.datastax.spark" %% "spark-cassandra-connector" % "3.1.0",
+  "joda-time" % "joda-time" % "2.10.14"
 )
--- src/main/scala/KafkaStreamProcessor.scala
+++ src/main/scala/KafkaStreamProcessor.scala
@@ -1,21 +1,85 @@
-import org.apache.spark.sql.SparkSession
+import com.datastax.oss.driver.api.core.uuid.Uuids
+import org.apache.spark.sql.cassandra.DataFrameWriterWrapper
+import org.apache.spark.sql.functions.{col, from_json, udf}
+import org.apache.spark.sql.streaming._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+final case class MultiPlayerScoreEvent(
+  playerId: Int,
+  playerName: String,
+  playerScore: Int
+)
 
 object KafkaStreamProcessor {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder
+      .master("local[*]")
       .appName("KafkaStreamProcessor")
       .getOrCreate
 
-    val df = spark
-      .readStream
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val df = spark.readStream
       .format("kafka")
-      .option("kafka.bootstrap.servers", "localhost:9090")
-      .option("subscribe", "topic1")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "kafka-spark-streaming")
+      .option("startingOffsets", "earliest")
       .load()
 
-    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-      .as[(String, String)]
+    val multiPlayerSchema = new StructType()
+      .add("playerId", IntegerType)
+      .add("playerName", StringType)
+      .add("playerScore", IntegerType)
+
+    import spark.implicits._
+    val playerScoreEvent = df
+      .selectExpr("CAST(value AS STRING)")
+      .select(from_json(col("value"), multiPlayerSchema).as("data"))
+      .select("data.*")
+      .map(row => MultiPlayerScoreEvent(row.getInt(0), row.getString(1), row.getInt(2)))
+
+    val makeUUID = udf(() => Uuids.timeBased().toString)
+
+    val summaryWithIDs = playerScoreEvent
+      .withColumn("id", makeUUID())
+      .withColumnRenamed("playerId", "player_id")
+      .withColumnRenamed("playerName", "player_name")
+      .withColumnRenamed("playerScore", "player_score")
+
+    // stream to console
+//    player.writeStream
+//      .format("console")
+//      .outputMode("append")
+//      .start()
+//      .awaitTermination()
+
+    // stream to cassandra
+    summaryWithIDs
+      .writeStream
+      .trigger(Trigger.ProcessingTime("5 seconds"))
+      .foreachBatch { (batchDF: DataFrame, batchID: Long) =>
+        println(s"Writing to Cassandra $batchID")
+        batchDF
+          .write
+          .cassandraFormat("multiplayer_score_event", "streaming")
+          .mode("append")
+          .save()
+      }
+      .outputMode("update")
+      .start()
+      .awaitTermination()
+
+    // another kafka topic
+//    df.selectExpr("CAST(id AS STRING) AS key", "to_json(struct(*)) AS value")
+//      .writeStream
+//      .format("kafka")
+//      .outputMode("append")
+//      .option("kafka.bootstrap.servers", "localhost:9092")
+//      .option("topic", "kafka-spark-streaming")
+//      .start()
+//      .awaitTermination()
   }
 }
